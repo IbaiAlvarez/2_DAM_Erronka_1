b5ae381a56ab07765499b0cb77fd2c0f29d6077f
Andoni <andoni.fernandezlo@elorrieta-errekamari.com>
2023-09-22 12:34:16 +0200

    Commit inicial

---
+++ app/src/main/java/com/example/erronka1/Eskaintzak.java
@@ -0,0 +1,17 @@
+package com.example.erronka1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Eskaintzak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eskaintzak);
+
+        Intent intent = getIntent();
+    }
+}
