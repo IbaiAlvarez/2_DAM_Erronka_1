cee4e849af727e693f8e5a98bea9d9eb4fdfff9e
Ibai <ibai.alvarezhe@elorrieta-errekamari.com>
2023-09-23 18:26:07 +0200

    Pantalla Login y Registro creadas y conexion a Firebase hecha

--- app/src/main/java/com/example/erronka1/Erregistroa.java
+++ app/src/main/java/com/example/erronka1/Erregistroa.java
@@ -1,14 +1,38 @@
 package com.example.erronka1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.w3c.dom.Text;
 
 public class Erregistroa extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_erregistroa);
+
+        EditText txt_izen_erregistro = findViewById(R.id.txt_izen_erregistro);
+        EditText txt_abizen_erregistro = findViewById(R.id.txt_abizen_erregistro);
+        EditText txt_email_erregistro = findViewById(R.id.txt_email_erregistro);
+        EditText txt_erabiltzaile_erregistro = findViewById(R.id.txt_erabiltzaile_erregistro);
+        EditText txt_pasahitza_erregistro = findViewById(R.id.txt_pasahitza_erregistro);
+        Button btn_erregistratu_erregistroa = findViewById(R.id.btn_erregistratu_erregistroa);
+        TextView lbl_logeatzea = findViewById(R.id.lbl_logeatzea);
+
+        lbl_logeatzea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Erregistroa.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/erronka1/Login.java
+++ app/src/main/java/com/example/erronka1/Login.java
@@ -7,52 +7,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
-    Button btn_registrar = findViewById(R.id.btn_registrar);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText edt_erabiltzaiela = findViewById(R.id.edt_erabiltzaiela);
-        EditText edt_pasahitza = findViewById(R.id.edt_pasahitza);
-        Button btn_iniciar = findViewById(R.id.btn_iniciar);
-        Button btn_registrar = findViewById(R.id.btn_registrar);
-        Button btn_ofertas = findViewById(R.id.btn_ofertas);
+        EditText txt_erabiltzailea = findViewById(R.id.txt_erabiltzailea);
+        EditText txt_pasahitza = findViewById(R.id.txt_pasahitza);
+        Button btn_login = findViewById(R.id.btn_login);
+        TextView lbl_erregistratzea = (TextView) findViewById(R.id.lbl_erregistratzea);
 
-        btn_ofertas.setOnClickListener(new View.OnClickListener() {
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent intent = new Intent(Login.this, Eskaintzak.class);
-
+                Intent intent = new Intent(Login.this, Hasiera.class);
                 startActivity(intent);
             }
         });
 
-        btn_registrar.setOnClickListener(new View.OnClickListener() {
+        lbl_erregistratzea.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Login.this, Erregistroa.class);
                 startActivity(intent);
             }
         });
-        btn_iniciar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Login.this, Eskaintzak.class);
-                startActivity(intent);
-            }
-        });
-
     }
-
-
-
-
-
-
 }
