29d66a96c12c2938edac78981bf4c3f0e884fab1
Ibai <ibaialvarezuni@gmail.com>
2023-09-25 12:28:39 +0200

    Conexion firebase funciona correctamente

--- app/src/main/java/com/example/erronka1/Login.java
+++ app/src/main/java/com/example/erronka1/Login.java
@@ -4,22 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 public class Login extends AppCompatActivity {
+
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Mensaje mensaje= new Mensaje("Hola Mundo!");
+        db.collection("message").document("Texto").set(mensaje);
+
         EditText txt_erabiltzailea = findViewById(R.id.txt_erabiltzailea);
         EditText txt_pasahitza = findViewById(R.id.txt_pasahitza);
         Button btn_login = findViewById(R.id.btn_login);
         TextView lbl_erregistratzea = (TextView) findViewById(R.id.lbl_erregistratzea);
+
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
