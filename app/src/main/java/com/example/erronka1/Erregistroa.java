326a9ce97a077f911224b83e970f92209a5542fe
Ibai <ibaialvarezuni@gmail.com>
2023-09-28 12:19:03 +0200

    Login y Registro comenzados a hacer y funcionan, falta hacer insert de los camposde Registro (crear objeto persona)

--- app/src/main/java/com/example/erronka1/Erregistroa.java
+++ app/src/main/java/com/example/erronka1/Erregistroa.java
@@ -1,23 +1,41 @@
 package com.example.erronka1;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
 public class Erregistroa extends AppCompatActivity {
 
 
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_erregistroa);
+
+        mAuth = FirebaseAuth.getInstance();
 
         EditText txt_izen_erregistro = findViewById(R.id.txt_izen_erregistro);
         EditText txt_abizen_erregistro = findViewById(R.id.txt_abizen_erregistro);
@@ -26,6 +44,33 @@ public class Erregistroa extends AppCompatActivity {
         EditText txt_pasahitza_erregistro = findViewById(R.id.txt_pasahitza_erregistro);
         Button btn_erregistratu_erregistroa = findViewById(R.id.btn_erregistratu_erregistroa);
         TextView lbl_logeatzea = findViewById(R.id.lbl_logeatzea);
+
+        btn_erregistratu_erregistroa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.createUserWithEmailAndPassword(txt_email_erregistro.getText().toString(), txt_pasahitza_erregistro.getText().toString())
+                    .addOnCompleteListener(Erregistroa.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+
+
+                                Intent intent = new Intent(Erregistroa.this, Hasiera.class);
+                                startActivity(intent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(Erregistroa.this, "Este correo ya esta registrado.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+            }
+        });
 
         lbl_logeatzea.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/erronka1/Login.java
+++ app/src/main/java/com/example/erronka1/Login.java
@@ -1,15 +1,25 @@
 package com.example.erronka1;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -18,27 +28,50 @@ public class Login extends AppCompatActivity {
 
     // Access a Cloud Firestore instance from your Activity
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Mensaje mensaje= new Mensaje("Hola Mundo!");
+        /*
         db.collection("message").document("Texto").set(mensaje);
+         */
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         EditText txt_erabiltzailea = findViewById(R.id.txt_erabiltzailea);
         EditText txt_pasahitza = findViewById(R.id.txt_pasahitza);
         Button btn_login = findViewById(R.id.btn_login);
         TextView lbl_erregistratzea = (TextView) findViewById(R.id.lbl_erregistratzea);
 
-
+        //Verifica Login del usuario
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Login.this, Hasiera.class);
-                startActivity(intent);
-            }
+
+                mAuth.signInWithEmailAndPassword(txt_erabiltzailea.getText().toString(), txt_pasahitza.getText().toString())
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                    Intent intent = new Intent(Login.this, Hasiera.class);
+                                    startActivity(intent);
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(Login.this, "Usuario o contraseña incorrectos.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                }
         });
 
         lbl_erregistratzea.setOnClickListener(new View.OnClickListener() {
